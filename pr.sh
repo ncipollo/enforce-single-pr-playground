511484d55ff52a99f5c2b55b347db2be5e92dd0e
Nick Cipollo <nicholas.cipollo@whoop.com>
2021-11-18 09:36:25 -0500

    update script

--- pr.sh
+++ pr.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-for i in {0..2}
+for i in {0..1}
 do
     branch_name=`uuidgen`
     
     printf "\n"
     printf ">>>Creating PR for branch $branch_name\n<<<"
     git checkout -b $branch_name
-    
+    date >> dates.txt
+    git add .
+    git commit -m "bump"
+    git push --set-upstream origin $branch_name
+
     gh pr create
     
     git checkout main
